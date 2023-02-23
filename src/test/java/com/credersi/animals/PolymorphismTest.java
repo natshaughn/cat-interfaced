bfa8808a0ec35d2980fc0781d009d55d080b0fdf
nathan.shaughnessy <nathan.shaughnessy@ROQ-319.roq.local>
2023-02-23 11:07:30 +0000

    Final push

--- src/test/java/com/credersi/animals/PolymorphismTest.java
+++ src/test/java/com/credersi/animals/PolymorphismTest.java
@@ -1,6 +1,7 @@
 package com.credersi.animals;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
