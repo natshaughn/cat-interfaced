9211164d3dc2a787b605860bc1851ee8e70114ac
nathan.shaughnessy <nathan.shaughnessy@ROQ-319.roq.local>
2023-02-23 10:07:03 +0000

    Committed some tests

--- src/main/java/com/credersi/animals/PlayChess.java
+++ src/main/java/com/credersi/animals/PlayChess.java
@@ -4,10 +4,3 @@ public interface PlayChess {
 	// Returns true or false
 	public boolean playChess();
 }
-
-//package com.credersi.animals;
-//
-//public interface Purr {
-//	// Returns loudness in decibels
-//	public int purr();
-//}
--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -12,4 +12,18 @@ public class CatTest {
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	@Test 
+	public void testChess() {
+		Parrot parrot = new Parrot();
+		Rhino rhino = new Rhino();
+		assertTrue(!parrot.playChess());
+		assertTrue(rhino.playChess());
+	}
+	@Test
+	public void testAverageAge() {
+		Parrot parrot = new Parrot();
+		Rhino rhino = new Rhino();
+		assertEquals(parrot.averageAge(), 40);
+		assertEquals(rhino.averageAge(), 50);
+	}
 }
